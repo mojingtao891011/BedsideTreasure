1be453b818c0f5e44d3614148290e72add504535
莫景涛 <13677785116@163.com>
2014-04-01 10:04:08 +0800

    Initial Commit

---
+++ BedsideTreasureTests/BedsideTreasureTests.m
@@ -0,0 +1,34 @@
+//
+//  BedsideTreasureTests.m
+//  BedsideTreasureTests
+//
+//  Created by 莫景涛 on 14-4-1.
+//  Copyright (c) 2014年 莫景涛. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BedsideTreasureTests : XCTestCase
+
+@end
+
+@implementation BedsideTreasureTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
